7f31fc7dec3fc286f53c1067e6f3e1110f963ec1
Frederik Wagner <typ1.diafreddy@gmail.com>
2026-08-14 10:54:36 +0200

    Stabilize Wear Robolectric test SDK

--- app-wear/src/test/kotlin/app/aapswear/wear/WearActivityTest.kt
+++ app-wear/src/test/kotlin/app/aapswear/wear/WearActivityTest.kt
@@ -10,8 +10,10 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [35])
 class WearActivityTest {
     @Test
     fun `dashboard exposes chart and bridge status`() {
