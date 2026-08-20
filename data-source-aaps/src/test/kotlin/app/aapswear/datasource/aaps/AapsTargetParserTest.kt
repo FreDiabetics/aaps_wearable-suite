c91818311ddc2ce22f7ecec59b0f5b012c54c3ad
Frederik Wagner <typ1.diafreddy@gmail.com>
2026-08-20 14:22:26 +0200

    Use project test API for target parser

--- data-source-aaps/src/test/kotlin/app/aapswear/datasource/aaps/AapsTargetParserTest.kt
+++ data-source-aaps/src/test/kotlin/app/aapswear/datasource/aaps/AapsTargetParserTest.kt
@@ -1,8 +1,8 @@
 package app.aapswear.datasource.aaps
 
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNull
-import org.junit.Test
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
 
 class AapsTargetParserTest {
     @Test
